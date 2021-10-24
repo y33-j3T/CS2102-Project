f53bf413b34c0414634990200fca5a670f2b16f7
y33-j3T <yeejet.tech@gmail.com>
2021-10-24 23:30:29 +0800

    add local data generation

--- data/Departments.sql
+++
@@ -1,5 +0,0 @@
-insert into Departments (did, dname) values (1, 'Business Development');
-insert into Departments (did, dname) values (2, 'Product Management');
-insert into Departments (did, dname) values (3, 'Business Development');
-insert into Departments (did, dname) values (4, 'Marketing');
-insert into Departments (did, dname) values (5, 'Research and Development');
