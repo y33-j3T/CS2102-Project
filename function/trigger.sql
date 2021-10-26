115585d2b83e7fe1edc5ce682494f2cb42b47362
Tan <e0550425@u.nus.edu>
2021-10-27 00:41:49 +0800

    Add remove if over capacity

--- function/trigger.sql
+++ function/trigger.sql
@@ -1,8 +1,48 @@
+DROP TRIGGER IF EXISTS remove_bookings_over_capacity ON Updates;
+CREATE TRIGGER remove_bookings_over_capacity
+    AFTER INSERT
+    ON Updates
+    FOR EACH ROW
+EXECUTE FUNCTION remove_bookings_over_capacity();
 CREATE OR REPLACE FUNCTION remove_bookings_over_capacity()
     RETURNS TRIGGER AS
 $$
-begin
+declare
+    curr_date        date := (SELECT CURRENT_DATE);
+    --Get the nearest update after this
+    next_update_date date := (SELECT date(U.datetime)
+                              FROM updates U
+                              WHERE U.datetime > NEW.datetime AND U.room = NEW.room AND U.floor = NEW.floor
+                              ORDER BY U.date
+                              LIMIT 1);
+    curs CURSOR FOR (SELECT S.time, S.date, S.floor, S.room, COUNT(J.eid)
+                     FROM sessions S
+                              JOIN Joins J
+                                   ON S.time = J.time AND S.date = J.date
+                                       AND S.floor = J.floor AND S.room = J.room
+                     WHERE S.floor = NEW.floor AND S.room = NEW.room
+                       AND (S.date >= curr_date -- Future Sessions
+                         AND (S.date >= NEW.date)) -- After the update date
 
+                     HAVING COUNT(J.eid) > NEW.new_cap);
+    r                RECORD;
+begin
+    open curs;
+    loop
+        -- EXIT WHEN NO MORE ROWS
+        fetch curs into r;
+        exit when not FOUND;
+        DELETE
+        FROM sessions S
+        WHERE S.time = r.time
+          AND S.date = r.date
+          AND S.floor = r.floor
+          AND S.room = r.room
+          AND (((next_update_date is not null) and S.date < next_update_date)
+                OR next_update_date is null);
+    end loop;
+    close curs;
+    return null;
 end;
 $$ LANGUAGE plpgsql;
 
@@ -70,7 +110,8 @@ begin
     can_join_meeting :=
                 (not is_meeting_approved(new.floor, new.room, new.time, new.date))
                 and (not is_having_fever(new.eid))
-                and (not is_resigned(new.eid));
+                and (not is_resigned(new.eid))
+                and is_under_max_capacity(new.floor, new.room, new.time, new.date);
     if can_join_meeting then
         return new;
     end if;
@@ -91,7 +132,7 @@ $$
 declare
     can_leave_meeting boolean;
 begin
-    can_leave_meeting :=  (not is_meeting_approved(old.floor, old.room, old.time, old.date));
+    can_leave_meeting := (not is_meeting_approved(old.floor, old.room, old.time, old.date));
     if can_leave_meeting then
         return old;
     end if;
