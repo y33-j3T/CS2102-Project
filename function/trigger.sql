50d44b4549be858ff966b10c53b3d6145aee8e58
y33-j3T <yeejet.tech@gmail.com>
2021-10-31 21:33:49 +0800

    move health trigger to trigger.sql

--- function/trigger.sql
+++ function/trigger.sql
@@ -150,3 +150,59 @@ CREATE TRIGGER can_leave_meeting
     FOR EACH ROW
 EXECUTE FUNCTION can_leave_meeting();
 
+
+-- Fever SOP:
+-- If employee is booker, delete Sessions where he booked, approved or not.
+-- Else remove the employee from all future Joins, approved or not.
+-- Get close contacts and do the same but for day D to day D+7 only
+CREATE OR REPLACE FUNCTION fever_sop() RETURNS TRIGGER AS $$
+DECLARE
+    fever_eid INTEGER;
+BEGIN
+    fever_eid := NEW.eid;
+
+    -- for fever employee
+    IF is_booker(fever_eid) THEN
+        -- employee is booker, delete Sessions where he booked
+        DELETE FROM Sessions
+        WHERE eid_booker = fever_eid
+        AND date > CURRENT_DATE;
+    ELSE
+        -- remove employee from all future Joins
+        DELETE FROM Joins j
+        WHERE j.eid = fever_eid
+        AND date > CURRENT_DATE;
+    END IF;
+
+    -- for close contacts
+    WITH CloseContacts AS (SELECT close_contact_eid FROM contact_tracing(fever_eid))
+    -- close contact is booker, delete Session he booked in day D to day D+7
+    DELETE FROM Sessions s
+    WHERE EXISTS (
+        SELECT 1
+        FROM CloseContacts c
+        WHERE s.eid_booker = c.close_contact_eid
+        AND s.date >= CURRENT_DATE
+        AND s.date <= CURRENT_DATE + 7
+    )
+    -- remove close contacts from Joins in day D to day D+7
+    DELETE FROM Joins j
+    WHERE EXISTS (
+        SELECT 1
+        FROM CloseContacts c
+        WHERE j.eid = c.close_contact_eid
+        AND j.date >= CURRENT_DATE
+        AND j.date <= CURRENT_DATE + 7
+    );
+
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- If an employee is declares fever at day D, activate fever SOP
+DROP TRIGGER IF EXISTS fever_detected ON HealthDeclaration;
+CREATE TRIGGER fever_detected
+AFTER INSERT ON HealthDeclaration
+FOR EACH ROW WHEN (NEW.fever = TRUE)
+EXECUTE FUNCTION fever_sop();
