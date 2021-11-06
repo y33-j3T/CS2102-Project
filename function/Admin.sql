c540ad8b5319ff292c6c668e755d1207afbb041f
hungvo0603 <hungvo1011@gmail.com>
2021-11-06 10:12:22 +0800

    Delete comments in admin.sql

--- function/Admin.sql
+++ function/Admin.sql
@@ -1,13 +1,4 @@
--- 1. non_compliance: This routine is used to find all employees that do not comply with the daily health declaration (i.e., to snitch). 
--- The inputs to the routine should minimally include:
-    -- Start date
-    -- End date
--- The routine returns a table containing all employee ID that do not declare their temperature at least once from the start date (inclusive) to the end date (inclusive). 
--- In other words, [start date, end date].
--- The table returned should minimally include the following columns:
-    -- Employee ID
-    -- Number of days
--- Number of days is the number of days the employee did not declare their temperature within the given period. The table should be sorted in descending order of number of days.
+-- non_compliance: find all employees that do not comply with the daily health declaration (i.e., to snitch). 
 CREATE OR REPLACE FUNCTION get_num_declared (IN start_date DATE, IN end_date DATE)
 RETURNS TABLE(eid INT, num_date INT) AS $$
 BEGIN
@@ -47,18 +38,7 @@ $$ LANGUAGE plpgsql;
 
 
 
--- 2. view_booking_report: This routine is to be used by employee to find all meeting rooms that are booked by the employee. 
--- The inputs to the routine should minimally include:
-    -- Start date
-    -- Employee ID
--- The routine returns a table containing all meeting rooms that are booked by the given employee as well as its approval status from the given start date onwards.
--- The table returned should minimally include the following columns:
-    -- Floor number 
-    -- Room number 
-    -- Date
-    -- Start hour
-    -- Is approved
--- The table should be sorted in ascending order of date and start hour.
+-- view_booking_report: used by employee to find all meeting rooms that are booked by the employee. 
 CREATE OR REPLACE FUNCTION view_booking_report (IN start_date DATE, IN eid INT)
 RETURNS TABLE(floor INT, room INT, date DATE, start_hr TEXT, is_approved BOOLEAN) AS $$
 DECLARE
@@ -90,17 +70,8 @@ $$ LANGUAGE plpgsql;
 
 
 
--- 3. view_future_meeting: This routine is to be used by employee to find all future meetings this employee is going to
--- have that are already approved. The inputs to the routine should minimally include:
-    -- Start date
-    -- Employee ID
--- The routine returns a table containing all meetings that are already approved for which this employee is joining from the given start date onwards. 
--- Note that the employee need not be the one booking this meeting room. The table returned should minimally include the following columns:
-    -- Floor number 
-    -- Room number 
-    -- Date
-    -- Start hour
--- The table should be sorted in ascending order of date and start hour.
+-- view_future_meeting: used by employee to find all future meetings this employee is going to
+-- have that are already approved.
 CREATE OR REPLACE FUNCTION view_future_meeting (IN start_date DATE, IN eid INT)
 RETURNS TABLE(floor INT, room INT, date DATE, start_hr TEXT) AS $$
 DECLARE
@@ -129,19 +100,7 @@ $$ LANGUAGE plpgsql;
 
 
 
--- 4. view_manager_report: This routine is to be used by manager to find all meeting rooms that require approval. The
--- inputs to the routine should minimally include: 
-    -- Start date
-    -- Employee ID
--- If the employee ID does not belong to a manager, the routine returns an empty table. 
--- Otherwise, the routine returns a table containing all meeting that are booked but not yet approved from the given start date onwards. 
--- Note that the routine should only return all meeting in the room with the same department as the manager. The table returned should minimally include the following columns:
-    -- Floor number 
-    -- Room number 
-    -- Date
-    -- Start hour 
-    -- Employee ID
--- The table should be sorted in ascending order of date and start hour.
+-- view_manager_report: used by manager to find all meeting rooms that require approval.
 CREATE OR REPLACE FUNCTION get_employee_department (IN eid INT)
 RETURNS INTEGER AS $$
 BEGIN
